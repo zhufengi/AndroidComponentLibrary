3f25f4121d07820a674fd86263a8c69c9be355b8
wangxiaotong <wang.xiaotong05382@westone.com.cn>
2018-10-11 19:22:14 +0800

    增加rxjava练习

--- app/src/main/java/com/zf/acl/SecondActivity.java
+++ app/src/main/java/com/zf/acl/SecondActivity.java
@@ -3,24 +3,53 @@ package com.zf.acl;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.zf.land.Logger;
 import com.zf.land.base.BaseActivity;
 import com.zf.land.eventbus.MessageEvent;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 
 public class SecondActivity extends BaseActivity {
 
-    private final String TAG = "SecondActivity";
+    private static final String TAG = "SecondActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 //        EventBusUtils.getInstance().post(new MessageEvent("MainActivity123556"));
+//        testObserver();
     }
 
     @Override
     public Object onMessageEvent(MessageEvent messageEvent) {
         Log.i(TAG,""+messageEvent.getMessage());
         return super.onMessageEvent(messageEvent);
+    }
+
+    public static Observer testObserver(){
+        return  new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(String o) {
+                Logger.log(TAG,"testObserver:"+o);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
     }
 }
---
+++ libcom/src/main/java/com/zf/land/Logger.java
@@ -0,0 +1,16 @@
+package com.zf.land;
+
+import android.util.Log;
+
+/**
+ * @author: wang.xiaotong
+ * @github: https://github.com/zhufengi
+ * @time: 2018/10/11
+ * @description: Logger
+ */
+public class Logger {
+
+    public static void log(String tag,String msg){
+        Log.i(tag,"当前线程>"+Thread.currentThread().getName()+"<"+msg);
+    }
+}
