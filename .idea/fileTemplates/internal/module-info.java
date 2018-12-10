83ddf3f1d7813705849b9a1aaf42420f2cb5c438
zhufengi <zhufeng_l@126.com>
2018-12-10 21:19:53 +0800

    add utils

--- .idea/fileTemplates/internal/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author: ${USER}
- * @github: https://github.com/zhufengi
- * @time: ${DATE}
- * @description: ${NAME}
- */
-#parse("File Header.java")
-module #[[$MODULE_NAME$]]# {
-}
--- .idea/fileTemplates/internal/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * @author: ${USER}
- * @github: https://github.com/zhufengi
- * @time: ${DATE}
- * @description: ${NAME}
- */
-#parse("File Header.java")
-#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
